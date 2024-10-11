f10344a505a33eb9b36c020973d4ae9033e0016e
anykey111 <rsamoilov@naver.com>
2024-10-11 15:42:48 +0300

    add README add test secript

---
+++ fa-test.cmd
@@ -0,0 +1,11 @@
+
+set WORKDIR=c:\ProgramData\FAForever\bin\
+cd %WORKDIR%
+echo %WORKDIR%
+
+start /B ForgedAlliance.exe /init init.lua /gpgnet 127.0.0.1:7237 /log fa1.log /nobugreport
+timeout 5
+start /B ForgedAlliance.exe /init init.lua /gpgnet 127.0.0.1:7237 /log fa2.log /nobugreport
+timeout 1
+start /B ForgedAlliance.exe /init init.lua /gpgnet 127.0.0.1:7237 /log fa3.log /nobugreport
+timeout 1
